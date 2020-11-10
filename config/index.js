7745764798e92ed0bc347d5472eff9f4fe70a8b4
Tania Gutierrez <riobuenodevelops@gmil.com>
2020-11-10 01:59:18 -0400

    config variables
    A config object with data from .env file

---
+++ config/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: __dirname + '/./../.env' });
+
+const config = {
+  dev: process.env.NODE_ENV !== 'production',
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT || '',
+  email: process.env.EMAIL,
+  emailPassword: process.env.EMAIL_PASSWORD,
+  credentialPath: process.env.CREDENTIAL_PATH,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
+};
+
+module.exports = { config };
