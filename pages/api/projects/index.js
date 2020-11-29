0e8b30d916896186587956d4e0895726ef237205
Tania Gutierrez <riobuenodevelops@gmil.com>
2020-11-29 16:26:27 -0400

    Fix: formating creator filter

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -15,41 +15,48 @@ export default authenticated(async function (req, res) {
       query: { name, creator, status },
     } = req;
 
-    scopeValidationHandler(['read:projects'], req, res, async function (
+    scopeValidationHandler(
+      ['read:projects'],
       req,
-      res
-    ) {
-      try {
-        const projects = await projectService.getProjects({
-          name,
-          creator,
-          status,
-        });
-        res.status(200).json(projects);
-      } catch (err) {
-        errorHandler(boom.internal(err), req, res);
-      }
-    });
-  } else if (method === 'POST') {
-    validationHandler(createProjectSchema, 'body', req, res, function (
-      req,
-      res
-    ) {
-      scopeValidationHandler(['create:projects'], req, res, async function (
-        req,
-        res
-      ) {
-        const { body: project } = req;
+      res,
+      async function (req, res) {
         try {
-          const createdProjectId = await projectService.createProject({
-            project,
+          const projects = await projectService.getProjects({
+            name,
+            creator,
+            status,
           });
-          res.status(201).json(createdProjectId);
+          res.status(200).json(projects);
         } catch (err) {
           errorHandler(boom.internal(err), req, res);
         }
-      });
-    });
+      }
+    );
+  } else if (method === 'POST') {
+    validationHandler(
+      createProjectSchema,
+      'body',
+      req,
+      res,
+      function (req, res) {
+        scopeValidationHandler(
+          ['create:projects'],
+          req,
+          res,
+          async function (req, res) {
+            const { body: project } = req;
+            try {
+              const createdProjectId = await projectService.createProject({
+                project,
+              });
+              res.status(201).json(createdProjectId);
+            } catch (err) {
+              errorHandler(boom.internal(err), req, res);
+            }
+          }
+        );
+      }
+    );
   } else {
     errorHandler(boom.methodNotAllowed(), req, res);
   }
